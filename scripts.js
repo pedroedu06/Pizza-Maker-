d8def9d7723ff643fbed3918be05fa46c210d10d
pedroedu06 <pedro.nascimento182501@gmail.com>
2025-01-26 13:27:56 -0300

    click x2 e aminacoes

--- scripts.js
+++ scripts.js
@@ -1,11 +1,14 @@
 
 //contador de pizza
 
-let pizzasCount = 400;
+let pizzasCount = 0;
+let pizzasperClick = 1;
 let doubleBought = 0;
 let tripleBought = 0;
+let clickx2Bought = 0;
 let maxDoubleBought = 5;
 let maxTripleBought = 3;
+let maxClickx2Bought = 1;
 
 const pizzamake = document.querySelector('#pizzamake');
 const count = document.querySelector('#count');
@@ -14,16 +17,18 @@ const count = document.querySelector('#count');
 
 const double = document.querySelector('#double');
 const triple = document.querySelector('#triple');
+const click2x = document.querySelector('#clickx2');
 const reset = document.querySelector('#resetAll');
 
 //minscore
 
 const minDouble = 150;
 const minTriple = 400;
-const resetAll = 2000000;
+const minClickX2 = 2000;
+const resetAll = 200000;
 
 function pizzaGame(){
-    pizzasCount ++;
+    pizzasCount += pizzasperClick;
     updatePizzas();
 }
 
@@ -41,6 +46,7 @@ function doublePower() {
     }
 }
 
+
 function triplePower(){
 
     if (pizzasCount >= minTriple && tripleBought < maxTripleBought){
@@ -54,8 +60,22 @@ function triplePower(){
         triple.disabled = true;
         window.alert("Atingiu o Limite de compra!")
     }
-
 }
+
+function doubleclickPower() {
+    if (pizzasCount >= minClickX2){
+        pizzasCount -= minClickX2;
+        pizzasperClick *= 2;
+        clickx2Bought ++;
+        updatePizzas();
+    }
+
+    if (clickx2Bought === maxClickx2Bought){
+        clicks2x.disabled = true;
+        window.alert("Atingiu o Limite de compra!")
+    }
+}
+
 function resetGame() {
     if (pizzasCount >= resetAll){
         pizzasCount = 0;
@@ -71,12 +91,34 @@ function updatePizzas(){
 
 //eventos de click
 
-pizzamake.addEventListener('click', pizzaGame);
+pizzamake.addEventListener('click', (e) =>{
+    pizzaGame();
+    createParticula(e.clientX, e.clientY)
+});
 double.addEventListener('click', doublePower);
 triple.addEventListener('click', triplePower);
+click2x.addEventListener('click', doubleclickPower)
 reset.addEventListener('click', resetGame);
 
-//click x2 
+//cookies up 
 
-const clicksx2 = document.querySelector('#clickx2');
-const clickx2 = 2000;
+const createParticula = (x,y) =>{
+    const pizzaClicks = document.querySelector('.pizza-clicks');
+    
+    const particulas = document.createElement("img");
+    particulas.setAttribute("src", "assets/icons8-pizza-100 (1).png");
+    particulas.setAttribute("class", "pizza-particle");
+    particulas.style.position = "absolute";
+    particulas.style.left = x + 'px';
+    particulas.style.right = y + 'px';
+
+    pizzaClicks.appendChild(particulas);
+
+    setTimeout(() =>{
+        pizzaClicks.removeChild(particulas);
+    }, 3000)
+}
+
+
+
+
